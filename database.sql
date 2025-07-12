a30ed760d862218d94c79867d4da374a49546994
Thio Van <thiovan99@gmail.com>
2025-07-12 16:47:15 +0700

    update database schema: add complaint_images and history_images tables, enhance statuses, and adjust data dumps

--- database.sql
+++ database.sql
@@ -12,6 +12,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Dumping database structure for simoni_db
+CREATE DATABASE IF NOT EXISTS `simoni_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `simoni_db`;
+
 -- Dumping structure for table simoni_db.complaints
 CREATE TABLE IF NOT EXISTS `complaints` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -27,9 +32,21 @@ CREATE TABLE IF NOT EXISTS `complaints` (
   UNIQUE KEY `ticket` (`ticket`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- Dumping data for table simoni_db.complaints: 2 rows
+-- Dumping data for table simoni_db.complaints: 6 rows
 /*!40000 ALTER TABLE `complaints` DISABLE KEYS */;
 /*!40000 ALTER TABLE `complaints` ENABLE KEYS */;
+
+-- Dumping structure for table simoni_db.complaint_images
+CREATE TABLE IF NOT EXISTS `complaint_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `complaint_id` int(11) DEFAULT NULL,
+  `filename` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table simoni_db.complaint_images: ~2 rows (approximately)
+/*!40000 ALTER TABLE `complaint_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `complaint_images` ENABLE KEYS */;
 
 -- Dumping structure for table simoni_db.histories
 CREATE TABLE IF NOT EXISTS `histories` (
@@ -41,38 +58,39 @@ CREATE TABLE IF NOT EXISTS `histories` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table simoni_db.histories: ~4 rows (approximately)
+-- Dumping data for table simoni_db.histories: ~17 rows (approximately)
 /*!40000 ALTER TABLE `histories` DISABLE KEYS */;
 /*!40000 ALTER TABLE `histories` ENABLE KEYS */;
 
--- Dumping structure for table simoni_db.images
-CREATE TABLE IF NOT EXISTS `images` (
+-- Dumping structure for table simoni_db.history_images
+CREATE TABLE IF NOT EXISTS `history_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `source_type` enum('complaint','history') DEFAULT NULL,
-  `source_id` int(11) DEFAULT NULL,
+  `history_id` int(11) DEFAULT NULL,
   `filename` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table simoni_db.images: 3 rows
-/*!40000 ALTER TABLE `images` DISABLE KEYS */;
-/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+-- Dumping data for table simoni_db.history_images: ~2 rows (approximately)
+/*!40000 ALTER TABLE `history_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `history_images` ENABLE KEYS */;
 
 -- Dumping structure for table simoni_db.statuses
 CREATE TABLE IF NOT EXISTS `statuses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
-  `order` varchar(50) DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Dumping data for table simoni_db.statuses: 4 rows
+-- Dumping data for table simoni_db.statuses: 6 rows
 /*!40000 ALTER TABLE `statuses` DISABLE KEYS */;
 INSERT INTO `statuses` (`id`, `name`, `order`) VALUES
-	(1, 'verifikasi', '1'),
-	(2, 'disposisi', '2'),
-	(3, 'tindak lanjut', '3'),
-	(4, 'selesai', '4');
+	(1, 'menunggu verifikasi', 0),
+	(2, 'ditolak', 0),
+	(3, 'verifikasi', 1),
+	(4, 'disposisi', 2),
+	(5, 'tindak lanjut', 3),
+	(6, 'selesai', 4);
 /*!40000 ALTER TABLE `statuses` ENABLE KEYS */;
 
 -- Dumping structure for table simoni_db.users
@@ -86,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Dumping data for table simoni_db.users: ~3 rows (approximately)
+-- Dumping data for table simoni_db.users: ~6 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `username`, `fullname`, `role`, `password`) VALUES
 	(1, 'admin', 'Admin', 'admin', '$2y$10$D7g6b9nyBXFhcu.vZBP7fO0H34LaeMMq5uv89OLa0KwId24PnHW/W'),
